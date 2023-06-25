ae35804b97a20ce58f4b34356c1f6f7a8b07bd48
Tim Holzhey <tholzhey.th@gmail.com>
2023-06-25 20:19:08 +0200

    Updated log levels

--- src/http/common.h
+++ src/http/common.h
@@ -24,17 +24,30 @@ typedef enum {
 #define LOG_COLOR_RED		"\033[31m"
 #define LOG_COLOR_BLUE		"\033[34m"
 
+#define LOG_LEVEL_ERROR			0
+#define LOG_LEVEL_INFO			1
+#define LOG_LEVEL_HIGHLIGHT		2
+#define LOG_LEVEL_DEBUG			3
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL LOG_LEVEL_GLOBAL
+#endif
+
 #if defined(DEBUG_GLOBAL) && DEBUG_GLOBAL == 1
 
-#define log_internal(color, ...) \
-	printf(color "%-20s: %-4d: ", FILENAME, __LINE__); \
-	printf(__VA_ARGS__); \
-	printf(LOG_COLOR_WHITE "\n");
+#define log_internal(color, level, ...) \
+	if (level <= LOG_LEVEL) { \
+		printf(color "%-20s: %-4d: ", FILENAME, __LINE__); \
+		printf(__VA_ARGS__); \
+		printf(LOG_COLOR_WHITE "\n"); \
+	}
 
-#define log_internal_raw(color, ...) \
-	printf(color); \
-	printf(__VA_ARGS__); \
-	printf(LOG_COLOR_WHITE);
+#define log_internal_raw(color, level, ...) \
+	if (level <= LOG_LEVEL) { \
+		printf(color); \
+		printf(__VA_ARGS__); \
+		printf(LOG_COLOR_WHITE); \
+	}
 
 #else
 
@@ -44,14 +57,15 @@ typedef enum {
 
 #endif
 
-#define log_info(...)			printf(__VA_ARGS__); printf("\n");
-#define log_debug(...)			log_internal(LOG_COLOR_WHITE, __VA_ARGS__)
-#define log_error(...)			log_internal(LOG_COLOR_RED, __VA_ARGS__)
-#define log_highlight(...)		log_internal(LOG_COLOR_BLUE, __VA_ARGS__)
+#define log_error(...)			log_internal(LOG_COLOR_RED, LOG_LEVEL_ERROR, __VA_ARGS__)
+#define log_info(...)           log_internal(LOG_COLOR_WHITE, LOG_LEVEL_INFO, __VA_ARGS__)
+#define log_highlight(...)		log_internal(LOG_COLOR_BLUE, LOG_LEVEL_HIGHLIGHT, __VA_ARGS__)
+#define log_debug(...)			log_internal(LOG_COLOR_WHITE, LOG_LEVEL_DEBUG, __VA_ARGS__)
 
-#define log_debug_raw(...)		log_internal_raw(LOG_COLOR_WHITE, __VA_ARGS__)
-#define log_error_raw(...)		log_internal_raw(LOG_COLOR_RED, __VA_ARGS__)
-#define log_highlight_raw(...)	log_internal_raw(LOG_COLOR_BLUE, __VA_ARGS__)
+#define log_raw_error(...)		log_internal_raw(LOG_COLOR_RED, LOG_LEVEL_ERROR, __VA_ARGS__)
+#define log_raw_info(...)       log_internal_raw(LOG_COLOR_WHITE, LOG_LEVEL_INFO, __VA_ARGS__)
+#define log_raw_highlight(...)	log_internal_raw(LOG_COLOR_BLUE, LOG_LEVEL_HIGHLIGHT, __VA_ARGS__)
+#define log_raw_debug(...)		log_internal_raw(LOG_COLOR_WHITE, LOG_LEVEL_DEBUG, __VA_ARGS__)
 
 #define RET_ON_FAIL(x) \
 	if ((x) != 0) { \
